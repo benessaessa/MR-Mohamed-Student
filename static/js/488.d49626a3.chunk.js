71744ccb5c0eb1e639adc9ed7a1756cd5ffa9e0b
benessaessa <moh.essa2020@gmail.com>
2026-03-08 23:27:02 +0200

    Updates

--- static/js/488.d49626a3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmr_mohamed_student=self.webpackChunkmr_mohamed_student||[]).push([[488],{488(e,s,t){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.d49626a3.chunk.js.map
